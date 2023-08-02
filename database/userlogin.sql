cccdb1e2b9ef7effbef1c20ac6673715c4299a4b
kswathi2709 <70970213+kswathi2709@users.noreply.github.com>
2023-08-02 23:07:16 +0530

    Add files via upload

---
+++ database/userlogin.sql
@@ -0,0 +1,15 @@
+create database userlogin;
+use userlogin;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (id, username, password)VALUES (1, 'swathi', 'starswak7');
+
+insert into users(id, username, password) values(2,"Adarsh","asus");
+
+select * from users;
